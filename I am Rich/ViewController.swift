b38e3210f643b7160460919f1e22fd8d84398141
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2022-10-17 13:00:33 -0400

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Gabriel Marquez on 2022-10-17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
